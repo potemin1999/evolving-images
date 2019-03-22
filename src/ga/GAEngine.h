99124e1a001dd1e8d97ebe534c5ee4a0ce85ed73
potemin1999 <potemin1999@bk.ru>
2019-03-22 10:30:25 +0300

    ga library compacted into one header file

--- src/ga/GAEngine.h
+++ src/ga/GAEngine.h
@@ -9,10 +9,83 @@
 #include <random>
 #include <unordered_map>
 #include <cstring>
-#include "GATypes.h"
 #include "Exception.h"
 
+
+#define GA_NAMESPACE_BEGIN namespace ga{
+#define GA_NAMESPACE_END }
+
 GA_NAMESPACE_BEGIN
+
+typedef double Fitness;
+
+template<typename Gene>
+class Engine;
+
+template<typename Gene>
+class Initializer;
+
+template<typename Gene>
+class Selector;
+
+template<typename Gene>
+class BreedSelector;
+
+template<typename Gene>
+class CrossoverExecutor;
+
+template<typename Gene>
+class Mutator;
+
+template<typename Gene>
+class Chromosome {
+private:
+    Gene *genes;
+    int genesCount;
+    Fitness fitness;
+
+public:
+    Chromosome() = default;
+
+    Chromosome(Gene *genes, int genesCount) {
+        this->setGenes(genes, genesCount);
+    }
+
+    virtual ~Chromosome() {
+        delete[] genes;
+    }
+
+    void setFitness(Fitness fitness) {
+        this->fitness = fitness;
+    }
+
+    void setGenes(Gene *gens, int genesCount) {
+        this->genes = genes;
+        this->genesCount = genesCount;
+    }
+
+    Fitness getFitness() const {
+        return this->fitness;
+    }
+
+    Gene *getGenes() const {
+        return this->genes;
+    }
+
+    int getGenesCount() const {
+        return this->genesCount;
+    }
+
+    int getBitCount() const {
+        return getGenesCount() * sizeof(Gene);
+    }
+};
+
+template<typename Gene>
+class FitnessFunction {
+public:
+    virtual Fitness apply(Chromosome<Gene> &chromosome) { return 0; };
+};
 
 class EngineComponent {
 protected:
